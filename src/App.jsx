464c582ad12d32b6d6d8280f88bde3782d8a802b
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-09-28 13:24:13 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Diablo 4</h1>
+      <div className="card">
+        <p>Powered By Vite + React</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
