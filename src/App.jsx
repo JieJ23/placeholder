3578396193dd7f63b3a606887d2afc3eef1e1f4e
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-10-03 18:33:16 -0400

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,62 @@
 export default function App() {
   return (
     <div className="h-lvh bg-[#171616]">
-      <div className="p-4 flex flex-wrap gap-2 justify-center">
-        <div className="card bg-primary image-full w-96 shadow-xl">
+      <div className="p-4 flex justify-center">
+        <div className="card bg-primary image-full w-[1000px] shadow-xl">
           <div className="card-body">
-            <h2 className="card-title">Box 1</h2>
+            <h2 className="card-title">Diablo Story</h2>
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              natus harum minima quia incidunt illo numquam dolorem odio,
-              facilis accusamus!
+              In Diablo 4, the story revolves around the rise of a dark force:
+              Lilith, the daughter of Mephisto, who has returned to the world of
+              Sanctuary. After being imprisoned for centuries, Lilith is
+              released by a cult and begins spreading chaos, influencing
+              humanity with her vision of freedom through destruction. Players
+              take on the role of a wanderer who gets caught in the conflict
+              between Lilith and the forces of Heaven and Hell. The world is
+              plunged into turmoil, with demonic invasions, civil unrest, and
+              broken alliances, as Lilith seeks to reshape Sanctuary in her
+              image. As the player ventures through the lands, they encounter
+              old characters from the series, such as Tyrael, and must battle
+              through the legions of demons and cultists to stop Lilith's dark
+              plans before the world succumbs to evil. The story is dark, filled
+              with moral choices and consequences, as Sanctuary teeters on the
+              edge of ruin.
             </p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Btn1 </button>
-            </div>
-          </div>
-        </div>
-        <div className="card bg-primary image-full w-96 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title">Box 2</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque hic
-              placeat commodi, esse dignissimos et!
-            </p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Btn2</button>
-            </div>
-          </div>
-        </div>
-        <div className="card bg-primary image-full w-96 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title">Box 3</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-              neque temporibus iure ipsam iusto laudantium dolorem possimus.
-              Vero officia quae porro quis quidem animi accusamus?
-            </p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Btn3</button>
-            </div>
           </div>
         </div>
       </div>
       <div className="mockup-code max-w-[800px] mx-auto p-4">
         <pre data-prefix="$">
-          <code>Testing Layout</code>
+          <code>All Games</code>
         </pre>
         <pre data-prefix=">" className="text-warning">
-          <code>Install Frame...</code>
+          <code>One (1997)</code>
         </pre>
         <pre data-prefix=">" className="text-warning">
-          <code>Config File...</code>
+          <code>Two (2000)</code>
         </pre>
         <pre data-prefix=">" className="text-warning">
-          <code>Install Plugin</code>
+          <code>Three (2021)</code>
         </pre>
-        <pre data-prefix=">" className="text-success">
-          <code>Done!</code>
+        <pre data-prefix=">" className="text-warning">
+          <code>Four (2023)</code>
         </pre>
+      </div>
+      <div className="p-4 flex justify-center">
+        <div className="card bg-success image-full w-[1000px] shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Latest News</h2>
+            <p>
+              The latest Diablo 4 expansion, Vessel of Hatred, is set for
+              release on October 8, 2024. This new chapter builds on the
+              character Neyrelle, who now holds Mephisto’s Soul Stone, and dives
+              deeper into her struggle as the demon attempts to influence her
+              actions. The expansion introduces a new region and a class called
+              Spiritborn, which allows players to mix and match skills from four
+              different spirit guardians.
+            </p>
+          </div>
+        </div>
       </div>
       <label className="form-control max-w-[800px] mx-auto mt-10">
         <div className="label">
